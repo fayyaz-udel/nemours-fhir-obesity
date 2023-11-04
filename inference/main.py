11bc5a47213217c2d4ee54b5876eff4ff72c1417
hamed <fayyaz@udel.edu>
2023-11-04 00:33:47 -0400

    checkpoint

--- inference/main.py
+++ inference/main.py
@@ -3,6 +3,12 @@ from math import expm1
 import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+import json
+from fhirclient.models.medicationrequest import MedicationRequest
+from fhirclient.models.patient import Patient
+from fhirclient.models.observation import Observation
+from fhirclient.models.bundle import Bundle
+from fhirclient.models.condition import Condition
 
 # from tensorflow import keras
 app = Flask(__name__)
@@ -15,7 +21,21 @@ cors = CORS(app)
 @cross_origin()
 def index():
     data = request.json
-    print(data)
+    medication_list = []
+    observation_list = []
+    condition_list = []
+    patient = None
+    for medication in data['medications']:
+        medication_list.append(MedicationRequest(medication["resource"]))
+    for observation in data['observations']:
+        observation_list.append(Observation(observation["resource"]))
+    for condition in data['conditions']:
+        condition_list.append(Condition(condition["resource"]))
+    patient = Patient(data['patient'])
+    #########################################################################
+    for medication in medication_list:
+        print(medication.medicationCodeableConcept.coding[0].code)
+
     # df = pd.DataFrame(data, index=[0])
     # prediction = model.predict(transformer.transform(df))
     predicted_bmi = 30  # expm1(prediction.flatten()[0])
--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,9 @@
-function get_preds() {
-
-    const data = {
-        "bmi": 20,
-        "weight": 150,
-        "height": 170,
-    };
+function get_preds(data) {
 
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:4000");
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(data));
+    xhr.send(JSON.stringify(data, null, 4));
     xhr.onload = function () {
         if (xhr.status === 200) {
             document.getElementById("preds").innerText = xhr.responseText;
@@ -18,181 +12,153 @@ function get_preds() {
         }
     };
 
-
 }
 
 
 function get_demographic() {
-
-    FHIR.oauth2.ready().then(function (client) {
-        client.patient.read().then(function (pt) {
+    return FHIR.oauth2.ready().then(function (client) {
+        return client.patient.read().then(function (pt) {
             document.getElementById("p_first_name").innerHTML = pt.name[0].given;
             document.getElementById("p_last_name").innerHTML = pt.name[0].family;
             document.getElementById("p_dob").innerHTML = pt.birthDate;
             document.getElementById("p_gender").innerHTML = pt.gender;
+            // Return the patient object to the next .then() in the chain
+            return pt;
         });
 
-    }).catch(console.error);
+    }).catch(function (error) {
+        console.error(error);
+        // Handle error or return a default value or promise rejection
+        throw error; // Rethrow to keep the promise chain in a rejected state
+    });
 }
 
+
 function get_meds() {
-    FHIR.oauth2.ready().then(function (client) {
-        client.patient.read().then(function (pt) {
-            // Get MedicationRequests for the selected patient
-            client.request("/MedicationRequest?patient=" + client.patient.id, {
+    return FHIR.oauth2.ready().then(function (client) {
+        return client.patient.read().then(function (pt) {
+            return client.request("/MedicationRequest?patient=" + client.patient.id, {
                 resolveReferences: ["medicationReference"],
                 graph: true,
             })
-                // Reject if no MedicationRequests are found
-                .then(function (data) {
-                    if (!data.entry || !data.entry.length) {
-                        throw new Error("No medications found for the selected patient");
-                    }
+            .then(function (data) {
+                if (!data.entry || !data.entry.length) {
+                    throw new Error("No medications found for the selected patient");
+                }
+                return data.entry;
+            })
+            .then(function (meds) {
+                // Create and return the table instead of just assigning to out_med
+                let table = document.createElement('table')
+                for (var j = 0; j < meds.length; j++) {
+                    let row = table.insertRow();
 
-                    return data.entry;
-                })
+                    let row_no = row.insertCell();
+                    row_no.textContent = j;
 
-                // Render the current patient's medications (or any error)
-                .then(
-                    function (meds) {
+                    let code = row.insertCell();
+                    code.textContent = meds[j]["resource"]["medicationCodeableConcept"]["coding"][0]["code"];
 
+                    let name = row.insertCell();
+                    name.textContent = meds[j]["resource"]["medicationCodeableConcept"]["coding"][0]["display"];
 
-                        let table = document.createElement('table')
-                        for (var j = 0; j < meds.length; j++) {
-                            let row = table.insertRow();
+                    let date = row.insertCell();
+                    date.textContent = meds[j]["resource"]["authoredOn"];
+                }
+                document.getElementById("med_head").insertAdjacentElement("afterend", table);
 
-                            let row_no = row.insertCell();
-                            row_no.textContent = j;
-
-                            let code = row.insertCell();
-                            code.textContent = meds[j]["resource"]["medicationCodeableConcept"]["coding"][0]["code"];
-
-                            let name = row.insertCell();
-                            name.textContent = meds[j]["resource"]["medicationCodeableConcept"]["coding"][0]["display"];
-
-                            let date = row.insertCell();
-                            date.textContent = meds[j]["resource"]["authoredOn"];
-
-                        }
-                        document.getElementById("med_head").insertAdjacentElement("afterend", table)
-                    },
-                    function (error) {
-                        document.getElementById("meds").innerText = error.stack;
-                    });
-        })
-    })
+                // Return the medication data for further processing if needed
+                return meds;
+            });
+        });
+    }).catch(function (error) {
+        // Handle or display the error
+        document.getElementById("meds").innerText = error.stack;
+        throw error; // Re-throw the error in case we have further chaining
+    });
 }
 
 function get_conds() {
-    FHIR.oauth2.ready().then(function (client) {
-        client.patient.read().then(function (pt) {
-            client.request("/Condition?patient=" + client.patient.id, {
+    return FHIR.oauth2.ready().then(function (client) {
+        return client.patient.read().then(function (pt) {
+            const patientId = pt.id;  // Assuming 'id' is the correct property
+
+            return client.request("/Condition?patient=" + patientId, {
                 resolveReferences: ["conditionReference"],
                 graph: true,
             })
-                .then(function (data) {
-                    if (!data.entry || !data.entry.length) {
-                        throw new Error("No conditions found for the selected patient");
-                    }
+            .then(function (data) {
+                if (!data.entry || !data.entry.length) {
+                    throw new Error("No conditions found for the selected patient");
+                }
+                return data.entry;
+            })
+            .then(function (conds) {
+                let table = document.createElement('table');
+                for (var j = 0; j < conds.length; j++) {
+                    let row = table.insertRow();
 
-                    return data.entry;
-                })
+                    let row_no = row.insertCell();
+                    row_no.textContent = j;
 
-                .then(
-                    function (conds) {
-                        let table = document.createElement('table')
-                        for (var j = 0; j < conds.length; j++) {
+                    let code = row.insertCell();
+                    code.textContent = conds[j]["resource"]["code"]["coding"][0]["code"];
 
-                            let row = table.insertRow();
+                    let name = row.insertCell();
+                    name.textContent = conds[j]["resource"]["code"]["coding"][0]["display"];
 
-                            let row_no = row.insertCell();
-                            row_no.textContent = j;
+                    let date = row.insertCell();
+                    date.textContent = conds[j]["resource"]["onsetDateTime"];
+                }
+                document.getElementById("cond_head").insertAdjacentElement("afterend", table);
 
-                            let code = row.insertCell();
-                            code.textContent = conds[j]["resource"]["code"]["coding"][0]["code"];
-
-                            let name = row.insertCell();
-                            name.textContent = conds[j]["resource"]["code"]["coding"][0]["display"];
-
-                            let date = row.insertCell();
-                            date.textContent = conds[j]["resource"]["onsetDateTime"];
-
-                        }
-                        document.getElementById("cond_head").insertAdjacentElement("afterend", table)
-
-                    },
-                    function (error) {
-                        document.getElementById("conds").innerText = error.stack;
-                    });
-
-        })
-    })
+                return conds;
+            });
+        });
+    }).catch(function (error) {
+        document.getElementById("conds").innerText = error.stack;
+        throw error;
+    });
 }
+
 
 
 function get_obsrvs() {
-    FHIR.oauth2.ready().then(function (client) {
-        client.patient.read().then(function (pt) {
+    return FHIR.oauth2.ready().then(function (client) {
+        return client.patient.read().then(function (pt) {
+            const patientId = pt.id; // Replace this with the correct way to get the patient's ID
 
-
-            // Get MedicationRequests for the selected patient
-            client.request("/Observation?patient=" + client.patient.id, {
+            return client.request(`/Observation?patient=${patientId}`, {
                 resolveReferences: ["medicationReference"],
                 graph: true,
             })
-                // Reject if no MedicationRequests are found
-                .then(function (data) {
-                    if (!data.entry || !data.entry.length) {
-                        throw new Error("No medications found for the selected patient");
-                    }
-
-                    return data.entry;
-                })
-
-                // Render the current patient's medications (or any error)
-                .then(
-                    function (obrvs) {
-
-
-                        let table = document.createElement('table')
-                        for (var j = 0; j < obrvs.length; j++) {
-
-
-                            if (!obrvs[j]["resource"]["code"]["coding"][0]["display"].includes("Body")) {
-                                continue;
-                            }
-
-                            let row = table.insertRow();
-
-                            let row_no = row.insertCell();
-                            row_no.textContent = j;
-
-                            let code = row.insertCell();
-                            code.textContent = obrvs[j]["resource"]["code"]["coding"][0]["code"];
-
-                            let name = row.insertCell();
-                            name.textContent = obrvs[j]["resource"]["code"]["coding"][0]["display"];
-
-                            let value = row.insertCell();
-
-                            if (obrvs[j]["resource"].hasOwnProperty('valueQuantity')) {
-                                value.textContent = obrvs[j]["resource"]["valueQuantity"]["value"];
-                            }
-
-                        }
-                        document.getElementById("obsrv_head").insertAdjacentElement("afterend", table)
-
-
-                    },
-                    function (error) {
-                        document.getElementById("meds").innerText = error.stack;
-                    });
-
-
-        })
-
-    })
+            .then(function (data) {
+                if (!data.entry || !data.entry.length) {
+                    throw new Error("No observations found for the selected patient");
+                }
+                return data.entry;
+            })
+            .then(function (obrvs) {
+                // It might be better to simply return the observations here
+                // and do the DOM manipulation elsewhere.
+                return obrvs.filter(obrv =>
+                    obrv["resource"]["code"]["coding"][0]["display"].includes("Body"));
+            });
+        });
+    }).catch(function (error) {
+        document.getElementById("obsrvs").innerText = error.stack; // Make sure this ID matches your error display element
+        throw error;
+    });
 }
+
+// Example usage:
+get_obsrvs().then(function (obrvs) {
+    // Do the DOM manipulation here with the filtered observations
+    console.log('Observations:', obrvs);
+}).catch(function (error) {
+    // Handle any errors here
+    console.error('An error occurred:', error);
+});
 
 
 function draw_chart() {
